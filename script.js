b0ca1766d43b627d738868533d6b963da8488f16
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-18 14:01:47 +0100

    create header, navigation searchbar elements, add filter function, event listener and episode state management

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//State Management
+let state = {
+  episodes: [],
+  searchTerm: "",
+};
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -5,6 +10,30 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.innerHTML = "";
+  //A header Element to contain searchbox element and a navigation bar for future navigation links
+  const headerSectionEl = document.createElement("section");
+  headerSectionEl.classList.add("header-section");
+  const navBarEl = document.createElement("nav");
+  navBarEl.classList.add("nav-bar");
+  headerSectionEl.appendChild(navBarEl);
+
+  //SearchBox element appended as a child of header element
+  const searchBoxEl = document.createElement("input");
+  searchBoxEl.type = "search";
+  searchBoxEl.placeholder = "Search episodes...";
+  headerSectionEl.appendChild(searchBoxEl);
+
+  //storing searchbox input value in state object
+   searchBoxEl.value = state.searchTerm;
+
+  //An event listener to to capture search event
+   searchBoxEl.addEventListener("input", (event) => {
+   const query = event.target.value;
+   state.searchTerm = query;
+   const filteredEpisodes = handleSearchInput(query, episodes);
+   makePageForEpisodes(filteredEpisodes);
+  });
+  rootElem.appendChild(searchBoxEl);
 
   episodeList.forEach((episode) => {
     const card = document.createElement("section");
@@ -35,6 +64,19 @@ function makePageForEpisodes(episodeList) {
     rootElem.appendChild(card);
   });
 
+  //Function to filter Episode
+  function handleSearchInput(query, episodeList) {
+    const searchTerm = query.trim().toLowerCase();
+    if (searchTerm === "") {
+      return episodeList;
+    }
+
+    return episodeList.filter((episode) => {
+      const name = episode.name.toLowerCase();
+      const summary = episode.summary ? episode.summary.toLowerCase() : "";
+      return name.includes(searchTerm) || summary.includes(searchTerm);
+    });
+  }
   // credit
   const credit = document.createElement("p");
   credit.innerHTML = `
