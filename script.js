1ce6b0d30f704e5ee35a3df324aca34985e62416
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-20 21:44:53 +0100

    disabled getAllepisodes data in episode.js file to allow data fetching, implement select episode functionality

--- script.js
+++ script.js
@@ -4,25 +4,30 @@ let state = {
   searchTerm: "",
   counterEl: null,
 };
-function setup() {
-  const allEpisodes = getAllEpisodes();
+const getAllEpisodes = async () => {
+  const response = await fetch("https://api.tvmaze.com/shows");
+  const data = await response.json();
+  return data;
+};
+async function setup() {
+  const allEpisodes = await getAllEpisodes();
   state.episodes = allEpisodes;
   makePageForEpisodes(state.episodes);
 }
-  //Function to filter Episode
-  function handleSearchInput(query, episodeList) {
-    const searchTerm = query.trim().toLowerCase();
-    if (searchTerm === "") {
-      return episodeList;
-    }
-
-    return episodeList.filter((episode) => {
-      const name = episode.name.toLowerCase();
-      const summary = episode.summary ? episode.summary.toLowerCase() : "";
-      return name.includes(searchTerm) || summary.includes(searchTerm);
-    });
+//Function to filter Episode
+function handleSearchInput(query, episodeList) {
+  const searchTerm = query.trim().toLowerCase();
+  if (searchTerm === "") {
+    return episodeList;
   }
-  
+
+  return episodeList.filter((episode) => {
+    const name = episode.name.toLowerCase();
+    const summary = episode.summary ? episode.summary.toLowerCase() : "";
+    return name.includes(searchTerm) || summary.includes(searchTerm);
+  });
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
 
@@ -47,7 +52,7 @@ function makePageForEpisodes(episodeList) {
   state.counterEl = counter;
 
   //SearchBox element appended as a child of header element
-  const searchEl = document.createElement("li")
+  const searchEl = document.createElement("li");
   const searchBoxEl = document.createElement("input");
   searchEl.appendChild(searchBoxEl);
   searchBoxEl.type = "search";
@@ -70,10 +75,23 @@ function makePageForEpisodes(episodeList) {
   const selectBoxEl = document.createElement("select");
   selectEl.appendChild(selectBoxEl);
   selectEl.classList.add("select-box");
-  navLinks.appendChild(selectEl)
-  
-  navBarEl.appendChild(navLinks);
+  navLinks.appendChild(selectEl);
 
+const displayOption = async () => {
+  const options = await getAllEpisodes();
+
+  for (const option of options) {
+    const newOption = document.createElement("option");
+    newOption.value = option.id; 
+    newOption.textContent = option.name;
+    selectBoxEl.appendChild(newOption);
+  }
+};
+
+displayOption();
+  
+
+  navBarEl.appendChild(navLinks);
   rootElem.appendChild(headerSectionEl);
 
   // credit
@@ -100,7 +118,7 @@ function renderEpisodes(episodeList) {
 
   episodeList.forEach((episode) => {
     const card = document.createElement("section");
-    card.classList.add("episode-card"); 
+    card.classList.add("episode-card");
 
     const season = String(episode.season).padStart(2, "0");
     const number = String(episode.number).padStart(2, "0");
@@ -132,3 +150,23 @@ function renderEpisodes(episodeList) {
 window.onload = setup;
 
 
+//   ## Adding new functionality
+
+// Level 400 is about expanding beyond one TV show.
+
+// Until now, your site has only showed information about the episode of one TV show.
+
+// But TVmaze has information about lots of TV shows, all in the same format.
+
+// We want to display any of them.
+
+// ### Requirements
+
+// 1. Add a `select` element to your page so the user can choose a show.
+// 2. When the user first loads the page, make a `fetch` request to https://api.tvmaze.com/shows ([documentation](https://www.tvmaze.com/api#show-index)) to get a list of available shows, and add an entry to the drop-down per show.
+// 3. When a user selects a show, display the episodes for that show, just like the earlier levels of this project.
+
+//   You will need to perform a `fetch` to get the episode list.
+// 4. Make sure that your search and episode selector controls still work correctly when you change shows.
+// 5. Your select must list shows in alphabetical order, case-insensitive.
+// 6. During one user's visit to your website, you should never fetch any URL more than once.
